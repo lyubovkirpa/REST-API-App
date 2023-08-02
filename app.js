fb21d9c814ab37d1dc4a693d9244be7a29636bf0
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-08-02 14:39:11 +0300

    start

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
+
+
+// const mongoose = require("mongoose");
+// const DB_HOST = "mongodb+srv://Liubov:7Qpu4z@CtVKK5GV@cluster0.p4lxqwj.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+// mongoose.set('strictQuery', true);
+
+// mongoose.connect(DB_HOST)
+//     .then(() => console.log("Database connect success"))
+//     .catch(error => console.log(error.message))
+
 
 const app = express();
 
@@ -22,3 +34,11 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message, stack: err.stack })
 })
 module.exports = app;
+
+
+// Liubov Kyrpa kirpa.lyubov.a@gmail.com password = fhdhiofk154
+// 7Qpu4z@CtVKK5GV
+
+// vfdjkfbhlffffh453
+
+// mongodb+srv://Liubov:<password>@cluster0.p4lxqwj.mongodb.net/
