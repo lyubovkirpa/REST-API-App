8a91e9d798a2417c4ab71cafe4fed226aea2e86f
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-08-06 00:01:31 +0300

    add logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -31,7 +31,7 @@ const register = async(req, res) => {
 const login = async(req, res) => {
   const {email, password} = req.body;
   const user = await User.findOne({email});
-  if (!user) {
+  if (!user || !user.token || user.token !== token) {
     throw HttpError(401, "Email or password is wrong");
   }
   const passwordCompare = await bcrypt.compare(password, user.password);
@@ -44,12 +44,28 @@ const login = async(req, res) => {
   }
 
   const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
+  await User.findByIdAndUpdate(user._id, {token});
 
   res.json({ token, })
 } 
+
+const getCurrent = async(req, res) => {
+  const {email, name} = req.user;
+
+  res.json({ email, name, })
+}
+
+const logout = async(req, res) => {
+  const {_id} = req.user;
+  await User.findByIdAndUpdate({_id, token: null});
+
+  res.json({ message: "No Content"})
+}
 
 
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
 }
