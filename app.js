bfc0c223f0d3a00fed369c273604d8c8a9d11138
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-08-15 16:22:56 +0300

    add sendEmail

--- app.js
+++ app.js
@@ -29,42 +29,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message, stack: err.stack });
 });
 
-// -----------------------------------------
-
-let ElasticEmail = require("@elasticemail/elasticemail-client");
-
-const { ELASTICEMAIL_API_KEY } = process.env;
-
-const defaultClient = ElasticEmail.ApiClient.instance;
-
-const { apikey } = defaultClient.authentications;
-apikey.apiKey = ELASTICEMAIL_API_KEY;
-
-const api = new ElasticEmail.EmailsApi();
-
-const email = ElasticEmail.EmailMessageData.constructFromObject({
-  Recipients: [new ElasticEmail.EmailRecipient("liubovkirpa@ukr.net")],
-  Content: {
-    Body: [
-      ElasticEmail.BodyPart.constructFromObject({
-        ContentType: "HTML",
-        Content: "<p>Verify email</p>",
-      }),
-    ],
-    Subject: "Verify email",
-    From: "kirpa.lyubov.a@gmail.com",
-  },
-});
-
-const callback = function (error, data, response) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log("API called successfully.");
-  }
-};
-api.emailsPost(email, callback);
-
-// ----------------------
-
 module.exports = app;
